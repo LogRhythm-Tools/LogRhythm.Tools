39d1b2444b3083928e338c1298568731ec5dfb67
Eric H <github.jtekt@gmail.com>
2020-07-21 18:25:27 -0600

    RecordedFuture Cmdlet Cleanup (#15)
    * Update New-RfExample.ps1
    
    Cleaned up example.  Added additional comments.
    
    * Update SmartResponse-RfThreatListSync.ps1
    
    Updated to reflect module change from SRF to LogRhythm.Tools
    
    * Delete SRP_RF_ThreatListSync.lpi
    
    Removed the compiled SRP.
    
    * Add examples for RF Domain cmdlets
    
    * Update Invoke-RfSync.ps1
    
    Reset list name prefix to default.
    
    * Update Get-RfHashRiskLists.ps1
    
    Update synopsis for RF cmdlets.
    
    * Update Get-RfIPRiskList.ps1
    
    Update synopsis for RF cmdlets.
    
    * Update Get-RfIPRiskLists.ps1
    
    Update synopsis for RF cmdlets.
    
    * Update Get-RfUrlRiskLists.ps1
    
    Update synopsis for RF cmdlets.
    
    * Update Get-RfVulnerabilityRiskList.ps1
    
    Update synopsis for RF cmdlets.
    
    * Update Get-RfVulnerabilityRiskLists.ps1
    
    Update synopsis for RF cmdlets.

--- examples/RecordedFuture/ThreatListSync/SmartResponse/SmartResponse-RfThreatListSync.ps1
+++ examples/RecordedFuture/ThreatListSync/SmartResponse/SmartResponse-RfThreatListSync.ps1
@@ -4,7 +4,7 @@ Param(
     [string]$Scope
 )
 Try {
-    Start-Job -InitializationScript { Import-Module SmartResponse.Framework } -ScriptBlock {Invoke-RfSync -SyncScope $args} -ArgumentList $Scope
+    Start-Job -InitializationScript { Import-Module LogRhythm.Tools } -ScriptBlock {Invoke-RfSync -SyncScope $args} -ArgumentList $Scope
     Write-Host "$(Get-TimeStamp) - Started: Invoke-RfSync Scope: $Scope"
 } Catch {
     Write-Host "Unable to start background Recorded Future Sync task.  Please run Invoke-RfSync manually on configured Sync asset."
