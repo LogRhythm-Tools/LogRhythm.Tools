bcb98afab266ea51efda2f77e96a21f4d21b6dc9
Gene Cupstid <56445225+GeneCupstid@users.noreply.github.com>
2020-08-29 19:13:29 -0500

    Update traces of "SmartResponse.Framework" + Remove old files in archive folder (#34)

--- src/Public/LogRhythm/Case/General/Get-LrCasePlaybooks.ps1
+++ src/Public/LogRhythm/Case/General/Get-LrCasePlaybooks.ps1
@@ -68,7 +68,7 @@ Function Get-LrCasePlaybooks {
     .NOTES
         LogRhythm-API
     .LINK
-        https://github.com/SmartResponse-Framework/SmartResponse.Framework        
+        https://github.com/LogRhythm-Tools/LogRhythm.Tools       
     #>
 
     [CmdletBinding()]
