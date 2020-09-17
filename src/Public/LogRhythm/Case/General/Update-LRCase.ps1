d34e8929e6d0b5aab83f68b050dc26cf064bcf62
Gene Cupstid <gene.cupstid@outlook.com>
2020-09-17 12:12:58 -0500

    Update Update-LRCase.ps1
    This version:
    1. fixes the DueDate offset issue
    2. has expanded example with object result
    3. only updates fields requested to be changed - if you only specify DueDate param, only DueDate is updated (the rest of the fields are omitted from the json body)
    4. Implements PassThru
    5. Includes the "Resolution" field

--- src/Public/LogRhythm/Case/General/Update-LRCase.ps1
+++ src/Public/LogRhythm/Case/General/Update-LRCase.ps1
@@ -1,33 +1,69 @@
 using namespace System
-using namespace System.IO
-using namespace System.Collections.Generic
 
 Function Update-LrCase {
     <#
     .SYNOPSIS
-        Update an existing case.
+        Update case information. For example, the case name, priority, and due date.
     .DESCRIPTION
-        The Update-LrCase cmdlet updates an existing case and returns the updated case record as a PSCustomObject.
+        The Update-LrCase cmdlet updates an existing case.
+    .PARAMETER Id
+        Unique identifier for the case, either as an RFC 4122 formatted string, or as
+        a number.
+    .PARAMETER ExternalId
+        Externally defined identifier for the case.
+    .PARAMETER Name
+        Name of the case.
+    .PARAMETER Priority
+        Priority of the case. Valid values: 1-5
+    .PARAMETER DueDate
+        When the case is due, either as an Rfc3339 formatted string, or as a [DateTime]
+    .PARAMETER Summary
+        Note summarizing the case.
+    .PARAMETER Resolution
+        Description of how the case was resolved.
     .PARAMETER Credential
         PSCredential containing an API Token in the Password field.
         Note: You can bypass the need to provide a Credential by setting
         the preference variable $LrtConfig.LogRhythm.ApiKey
         with a valid Api Token.
-    .PARAMETER Name
-        Name of the case.
-    .PARAMETER Priority
-        (int 1-5) Priority of the case.
-    .PARAMETER DueDate
-        When the case is due, as [System.DateTime] or a date-parsable [System.String]
-        If ommitted, a due date will be set for 24 hours from the current time.
-    .PARAMETER Summary
-        Note summarizing the case.
     .INPUTS
-        Name, Priority, DueDate, and Summary can all be sent by name through the pipeline.
+        [System.Object]   ->  Id
     .OUTPUTS
-        PSCustomObject representing the newly created case.
+        If the PassThru switch is provided, a PSCustomObject representing the modified
+        LogRhythm Case will be returned.
     .EXAMPLE
-        PS C:\> Update-LrCase -Id 5 -Name "test" -Priority 5 -Summary "test summary" -DueDate "10-20-2020 14:22:11"
+        PS C:\> Update-LrCase -Id 4481 -Summary "My New Summary"
+        ---
+        In this example we updated the Summary of Case number 4481 to "My New Summary".
+        As we did not include the PassThru parameter, there will be no output.
+    .EXAMPLE
+        In this example we will use the pipeline to pass a case object to Update-LrCase
+        and update every field we can.
+        
+        
+        PS C:\> $case = Get-LrCaseById -Id 4481
+
+        PS C:\> $case | Update-LrCase -ExternalId "Ext1212" -Name "Example Case" -Priority 3 -DueDate 2020-11-01 -Summary "Here's my summary" -Resolution "a resolution!" -PassThru
+
+        ---
+        id                      : ACAA4DF4-E810-4AF5-A3FA-2B3BA47BD237
+        number                  : 4481
+        externalId              : Ext1212
+        dateCreated             : 2020-09-16T12:11:38.1893786Z
+        dateUpdated             : 2020-09-17T09:58:21.8816109Z
+        dateClosed              :
+        owner                   : @{number=11; name=API, Example; disabled=False}
+        lastUpdatedBy           : @{number=11; name=API, Example; disabled=False}
+        name                    : Example Case
+        status                  : @{name=Created; number=1}
+        dueDate                 : 2020-11-01T05:00:00Z
+        resolution              : a resolution!
+        resolutionDateUpdated   : 2020-09-17T09:58:21.8816109Z
+        resolutionLastUpdatedBy : @{number=11; name=API, Example; disabled=False}
+        summary                 : Here's my summary
+        entity                  : @{number=-100; name=Global Entity; fullName=Global Entity}
+        collaborators           : {@{number=11; name=API, Example; disabled=False}, @{number=19; name=Smith, Bob; disabled=False}}
+        tags                    : {}
     .NOTES
         LogRhythm-API
     .LINK
@@ -36,35 +72,61 @@ Function Update-LrCase {
 
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory=$false, Position=0)]
+        [Parameter(
+            Mandatory = $true, 
+            ValueFromPipeline = $true, 
+            ValueFromPipelineByPropertyName = $true, 
+            Position = 0
+        )]
         [ValidateNotNull()]
-        [pscredential] $Credential = $LrtConfig.LogRhythm.ApiKey,
-
-        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName = $true, Position=1)]
         [object] $Id,
 
-        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName = $true, Position=2)]
+        
+        [Parameter(Mandatory = $false, Position = 1)]
+        [ValidateLength(0,250)]
+        [string] $ExternalId,
+        
+
+        [Parameter(Mandatory = $false, Position = 2)]
         [ValidateLength(1,250)]
         [string] $Name,
 
-        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName = $true, Position=3)]
+
+        [Parameter(Mandatory = $false, Position = 3)]
         [ValidateRange(1,5)]
         [int] $Priority,
 
-        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, Position = 4)]
+
+        [Parameter(Mandatory = $false, Position = 4)]
         [DateTime] $DueDate,
 
-        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, Position = 5)]
-        [ValidateLength(1,10000)]
-        [string] $Summary
+
+        [Parameter(Mandatory = $false, Position = 5)]
+        [ValidateLength(0,10000)]
+        [string] $Summary,
+
+
+        [Parameter(Mandatory = $false, Position = 6)]
+        [ValidateLength(0,500)]
+        [string] $Resolution,
+
+
+        [Parameter(Mandatory = $false, Position = 8)]
+        [switch] $PassThru,
+
+
+        [Parameter(Mandatory = $false, Position = 9)]
+        [ValidateNotNull()]
+        [pscredential] $Credential = $LrtConfig.LogRhythm.ApiKey
     )
+
 
     Begin {
         $Me = $MyInvocation.MyCommand.Name
-
+        
         $BaseUrl = $LrtConfig.LogRhythm.CaseBaseUrl
         $Token = $Credential.GetNetworkCredential().Password
-        
+
         # Enable self-signed certificates and Tls1.2
         Enable-TrustAllCertsPolicy
 
@@ -75,80 +137,96 @@ Function Update-LrCase {
 
         # Request Method
         $Method = $HttpMethod.Put
+
+        # Set initial ProcessedCount
+        $ProcessedCount = 0
     }
 
+
     Process {
-        # Test CaseID Format
+        # Test CaseId Format
         $IdStatus = Test-LrCaseIdFormat $Id
         if ($IdStatus.IsValid -eq $true) {
             $CaseNumber = $IdStatus.CaseNumber
         } else {
             return $IdStatus
-        } 
-
-        $CaseDetails = Get-LrCaseById -Id $CaseNumber
-
-        if ($Name) {
-            $_name = $Name
-        } else {
-            $_name = $CaseDetails.Name
         }
 
-        if ($DueDate) {
-            $_dueDate = $DueDate
-        } else {
-            $_dueDate = $CaseDetails.dueDate
-        }
-
-        if ($Priority) {
-            $_priority = $Priority
-        } else {
-            $_priority = $CaseDetails.priority
-        }
-
-        if ($Summary) {
-            $_summary = $Summary
-        } else {
-            $_summary = $CaseDetails.summary
-        }
-
+        # Request URI
         $RequestUrl = $BaseUrl + "/cases/$CaseNumber/"
 
-        # Request Body
-        $Body = [PSCustomObject]@{
-            name = $_name
-            priority = $_priority
-            externalId = $null
-            dueDate = [Xml.XmlConvert]::ToString(($_dueDate),[Xml.XmlDateTimeSerializationMode]::Utc)
-            summary = $_summary
-        }
-        $Body = $Body | ConvertTo-Json
 
-        # Send Request
-        if ($PSEdition -eq 'Core'){
+        #region: Create Json Body                                                                  
+        $Body = [PSCustomObject]@{}
+        
+        # Parameter: ExternalId
+        if (! [string]::IsNullOrEmpty($ExternalId)) {
+            $Body | Add-Member -MemberType NoteProperty -Name "externalId" -Value $ExternalId
+        }
+
+        # Parameter: Name
+        if (! [string]::IsNullOrEmpty($Name)) {
+            $Body | Add-Member -MemberType NoteProperty -Name "name" -Value $Name
+        }
+
+        # Parameter: Priority
+        if ($Priority -gt 0) {
+            $Body | Add-Member -MemberType NoteProperty -Name "priority" -Value $Priority
+        }
+
+        # Parameter: DueDate
+        if ($DueDate) {
+            $_dueDate = ($DueDate.ToUniversalTime()).ToString("yyyy-MM-ddTHH:mm:ssZ")
+            $Body | Add-Member -MemberType NoteProperty -Name "dueDate" -Value $_dueDate
+        }
+
+        # Parameter: Summary
+        if (! [string]::IsNullOrEmpty($Summary)) {
+            $Body | Add-Member -MemberType NoteProperty -Name "summary" -Value $Summary
+        }
+
+        # Parameter: Resolution
+        if (! [string]::IsNullOrEmpty($Resolution)) {
+            $Body | Add-Member -MemberType NoteProperty -Name "resolution" -Value $Resolution
+        }
+
+        # Important - Convert to JSON 
+        $Body = $Body | ConvertTo-Json
+        #endregion
+        
+
+        #region: Send Request                                                                      
+        Write-Verbose "[$Me]: request body is:`n$Body"
+
+        if ($PSEdition -eq 'Core') {
             try {
                 $Response = Invoke-RestMethod $RequestUrl -Headers $Headers -Method $Method -Body $Body -SkipCertificateCheck
-                Write-Verbose "[$Me]: Created Case $($Response.id)" 
-            }
-            catch {
+            } catch {
                 $Err = Get-RestErrorMessage $_
                 throw [Exception] "[$Me] [$($Err.statusCode)]: $($Err.message) $($Err.details)`n$($Err.validationErrors)`n"
             }
         } else {
             try {
                 $Response = Invoke-RestMethod $RequestUrl -Headers $Headers -Method $Method -Body $Body
-                Write-Verbose "[$Me]: Created Case $($Response.id)"
             }
             catch [System.Net.WebException] {
                 $Err = Get-RestErrorMessage $_
                 throw [Exception] "[$Me] [$($Err.statusCode)]: $($Err.message) $($Err.details)`n$($Err.validationErrors)`n"
             }
         }
+        #endregion
+        
 
-        # Done!
-        return $Response
+        $ProcessedCount++
+
+        # Return
+        if ($PassThru) {
+            return $Response
+        }
     }
 
-
-    End { }
+    
+    End { 
+        Write-Verbose "Updated $ProcessedCount cases."
+    }
 }
