73400640f6fa26aa411ca4627d16edd3e55a08a7
Jt3kt <github.jtekt@gmail.com>
2020-10-29 17:12:09 -0400

    New Cmdlet: New-LrCaseHelper
    This cmdlet is responsible for generating a new LogRhythm Case and combining the use of several inner-logrhythm APIs to facilitate simple execution with mature results.
    
    Supports auto-generating tags for assets, users, and one default tag.
    Supports bringing in a Playbook.
    Supports identifying if a case is existing in an open, incident, mitigated status that holds the same Alarm Name + matched tag involving one of (defined) User, HostOrigin, HostImpacted.
    Returns back as a cmdlet result all of the appropriate data points from the combined API services to support any additional custom SRP actions.

---
+++ src/Public/LogRhythm/Case/Helpers/New-LrTagTaxObject.ps1
@@ -0,0 +1,32 @@
+# Helper function to build LR Taxonomy Tags
+Function New-LrTagTaxObject {
+    [CmdletBinding()]
+    Param(
+        $Primary,
+        $Secondary,
+        $Tertiary,
+        $Value,
+        $Note
+    )
+    $OutputObject = [PSCustomObject]@{
+        Primary = $Primary
+        Secondary = $Secondary
+        Tertiary = $Tertiary
+        Value = $Value
+        Tag = $null
+        Note = $Note
+    }
+
+    # Establish Complete Tag
+    if ($Secondary -and $Tertiary) { 
+        $TagComplete = $Primary+"_"+$Secondary+"_"+$Tertiary+"_"+$Value
+    } elseif ($Secondary) {
+        $TagComplete = $Primary+"_"+$Secondary+"_"+$Value
+    } else {
+        $TagComplete = $Primary+"_"+$Value
+    }
+
+    $OutputObject.Tag = $TagComplete
+
+    return $OutputObject
+}
