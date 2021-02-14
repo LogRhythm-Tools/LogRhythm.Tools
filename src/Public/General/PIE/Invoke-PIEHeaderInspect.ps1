79ecd5c3061c3981a4df9b792d5b93c6b5e6e70e
Eric H <github.jtekt@gmail.com>
2021-02-14 09:27:10 -0700

    Update Invoke-PIEHeaderInspect.ps1
    Initial PIE Header Inspection module to parse out Header content into an enrichment state.

--- src/Public/General/PIE/Invoke-PIEHeaderInspect.ps1
+++ src/Public/General/PIE/Invoke-PIEHeaderInspect.ps1
@@ -28,25 +28,24 @@ function Invoke-PIEHeaderInspect {
         # by y
         $Regex_Received2 = '^by\s+(?<byhost>(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9]))(\s\((?<byaddr>(?<bIPv6>([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))|(?<bIPv4>\d{1,3}(\.\d{1,3}){3}))\))?\s+.*,\s(?<date>(([0-9])|([0-2][0-9])|([3][0-1]))\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s\d{4})\s(?<time>[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}).*$'
     
-        # Key fields: 
-        # x-ms-exchange-organization-originalclientipaddress <public>
-        # x-ms-exchange-organization-originalserveripaddress <private>
-        #  authentication-results
-        # dkin-signature
+
+        $Regex_SPFStatus = [list[object]]::new()
+        $Regex_SPFStatus.Add(@{Status="Pass";Regex=@('^(.*\s)?(?<result>pass)\s.*');Risk=0})
+        $Regex_SPFStatus.Add(@{Status="Fail";Regex=@('^(.*\s)?(?<result>fail)\s.*');Risk=3})
+        $Regex_SPFStatus.Add(@{Status="SoftFail";Regex=@('^(.*\s)?(?<result>softfail)\s.*');Risk=2})
+        $Regex_SPFStatus.Add(@{Status="Neutral";Regex=@('^(.*\s)?(?<result>neutral)\s.*');Risk=0})
+        $Regex_SPFStatus.Add(@{Status="None";Regex=@('^(.*\s)?(?<result>none)\s.*');Risk=0})
+        $Regex_SPFStatus.Add(@{Status="Permanent Error";Regex=@('^(.*\s)?(?<result>permerror)\s.*');Risk=1})
+        $Regex_SPFStatus.Add(@{Status="Temporary Error";Regex=@('^(.*\s)?(?<result>temperror)\s.*');Risk=1})
     }
 
     Process {
         # Object that represents the data points for URL Shortlink services
         $Header_Details = [PSCustomObject]@{
-            received = $null
             mimeversion = $null
-            dkim = [PSCustomObject]@{
-                signature = $null
-                verdict = $null
-            }
             microsoft = [pscustomobject]@{}
         }
-
+        <#
         # Process Received
         $ReceivedOutput = [List[object]]::new()
         $ReceivedData = $Headers | Where-Object -Property 'field' -like 'Received' | Sort-Object -Property Offset
@@ -71,7 +70,7 @@ function Invoke-PIEHeaderInspect {
                 timestamp = $null
                 
             }
-
+            
             $Received1 = ([regex]::Matches($ReceivedData[$i].Value, $Regex_Received1))
             if ($Received1) {
                 ForEach ($RegexMatch in $Received1) {
@@ -138,7 +137,7 @@ function Invoke-PIEHeaderInspect {
 
         # Add data to HeaderOutput
         $Header_Details.Received = $ReceivedOutput
-
+        #>
 
         # If the record contains x-forefront-antispam-report
         if ($Headers.field -contains 'x-forefront-antispam-report') {
@@ -217,7 +216,14 @@ function Invoke-PIEHeaderInspect {
 
         # If the record contains Message-Id
         if ($Headers.field -contains 'message-id') {
-            $Header_Details | add-member -membertype NoteProperty "message-id" -Value $($Headers | Where-Object -Property 'field' -like 'message-id' | Select-Object -ExpandProperty value)
+            $HeaderMessageId = $($Headers | Where-Object -Property 'field' -like 'message-id' | Select-Object -ExpandProperty value)
+            if ($HeaderMessageId -like "<*") {
+                $HeaderMessageId = $HeaderMessageId.Replace("<","")
+            }
+            if ($HeaderMessageId -like "*>") {
+                $HeaderMessageId = $HeaderMessageId.Replace(">","")
+            }
+            $Header_Details | add-member -membertype NoteProperty "message-id" -Value $HeaderMessageId
         }
 
         # If the record contains List-Unsubscribe
@@ -329,13 +335,170 @@ function Invoke-PIEHeaderInspect {
 
         # If the record contains received-spf
         if ($Headers.field -contains 'received-spf') {
-            $Header_Details | add-member -membertype NoteProperty "received-spf" -Value $($Headers | Where-Object -Property 'field' -like 'received-spf' | Select-Object -ExpandProperty value)
+            $SPFDetails = [PSCustomObject]@{
+                value = $($Headers | Where-Object -Property 'field' -like 'received-spf' | Select-Object -ExpandProperty value)
+                status = $null
+                summary = $null
+                risk = 0
+            }
+
+            ForEach ($SPFStatus in $Regex_SPFStatus) {
+                ForEach ($RegexPattern in $SPFStatus.regex) {
+                    $SPFVerdict = $SPFDetails.value -Match $RegexPattern
+                    if ($SPFVerdict -eq $True) {
+                        break
+                    }
+                }
+                if ($SPFVerdict -eq $true) {
+                    $SPFDetails.status = $SPFStatus.Status
+                    $SPFDetails.risk = $SPFStatus.Risk
+                    break
+                }
+            }
+
+            $SPFSummary = ([regex]::Matches($SPFDetails.value, '^.*\s\((?<summary>.*)\).*$'))
+
+            if ($SPFSummary[0].Groups['summary'].Value) {
+                $SPFDetails.Summary = $SPFSummary[0].Groups['summary'].Value
+            }
+
+
+            $Header_Details | add-member -membertype NoteProperty "received-spf" -Value $SPFDetails
         }
 
+        # If the record contains dkim-signature
+        <#
+        if ($Headers.field -contains 'dkim-signature') {
+            $DKIMDetails = [PSCustomObject]@{
+                algorithm = $null
+                body_hash = $null
+                canonical_algorithm = $null
+                domain = $null
+                header_fields = $null
+                query_method = $null
+                selector = $null
+                signature = $null
+                timestamp = $null
+                value = $($Headers | Where-Object -Property 'field' -like 'dkim-signature' | Select-Object -ExpandProperty value)
+                version = $null
+            }
+
+            # DKIM Version
+            $DKIMVersion = ([regex]::Matches($DKIMDetails.value, '^v=(?<version>\d+);.*'))
+            if ($DKIMVersion[0].Groups['version'].Value) {
+                $DKIMDetails.version = $DKIMVersion[0].Groups['version'].Value
+            }
+
+            # Algorithm
+            $DKIMAlgorithm = ([regex]::Matches($DKIMDetails.value, '^.*a=(?<algorithm>\S+);.*'))
+            if ($DKIMAlgorithm[0].Groups['algorithm'].Value) {
+                $DKIMDetails.algorithm = $DKIMAlgorithm[0].Groups['algorithm'].Value
+            }
+
+            # Query Method
+            $DKIMQMethod = ([regex]::Matches($DKIMDetails.value, '^.*q=(?<qmethod>\S+);.*'))
+            if ($DKIMQMethod[0].Groups['qmethod'].Value) {
+                $DKIMDetails.query_method = $DKIMQMethod[0].Groups['qmethod'].Value
+            }
+
+            # Canonical Algorithm
+            $DKIMCAlgorithm = ([regex]::Matches($DKIMDetails.value, '^.*c=(?<calgorithm>\S+);.*'))
+            if ($DKIMCAlgorithm[0].Groups['calgorithm'].Value) {
+                $DKIMDetails.canonical_algorithm = $DKIMCAlgorithm[0].Groups['calgorithm'].Value
+            }
+
+            # Selector
+            $DKIMSelector = ([regex]::Matches($DKIMDetails.value, '^.*s=(?<selector>\S+);.*'))
+            if ($DKIMSelector[0].Groups['selector'].Value) {
+                $DKIMDetails.selector = $DKIMSelector[0].Groups['selector'].Value
+            }
+
+            # Domain
+            $DKIMDomain = ([regex]::Matches($DKIMDetails.value, '^.*d=(?<domain>\S+);.*'))
+            if ($DKIMDomain[0].Groups['domain'].Value) {
+                $DKIMDetails.domain = $DKIMDomain[0].Groups['domain'].Value
+            }
+
+            # Timestamp
+            $DKIMTime = ([regex]::Matches($DKIMDetails.value, '^.*t=(?<time>\S+);.*'))
+            if ($DKIMTime[0].Groups['time'].Value) {
+                # Maintain UnixEpoch and add converted timestamp.
+                $DKTime = [PSCustomObject]@{
+                    original = $DKIMTime[0].Groups['time'].Value
+                    modified = $(ConvertFrom-UnixEpoch -UnixTime $($DKIMTime[0].Groups['time'].Value))
+                }
+                $DKIMDetails.timestamp = $DKTime
+            }
+
+            # Header Fields
+            $DKIMHFields = ([regex]::Matches($DKIMDetails.value, '^.*;(\s+)?h=(?<hfields>\S+);.*'))
+            if ($DKIMHFields[0].Groups['hfields'].Value) {
+                $DKIMDetails.header_fields = $DKIMHFields[0].Groups['hfields'].Value
+            }
+
+            # Body Hash
+            $DKIMBHash = ([regex]::Matches($DKIMDetails.value, '^.*;(\s+)?bh=(?<bhash>\S+);.*'))
+            if ($DKIMBHash[0].Groups['bhash'].Value) {
+                $DKIMDetails.body_hash = $DKIMBHash[0].Groups['bhash'].Value
+            }
+
+            
+            # Signature of Headers and Body
+            $DKIMSignature = ([regex]::Matches($DKIMDetails.value, '^.*;(\s+)?b=(?<signature>.*)$'))
+            if ($DKIMSignature[0].Groups['signature'].Value) {
+                $DKIMDetails.signature = $DKIMSignature[0].Groups['signature'].Value
+            }
+
+            # DNS Lookup for $selector._domainkey.$domain
+            $DKIMDNSResolve = Resolve-DnsName -Name "$($DKIMDetails.selector)._domainkey.$($DKIMDetails.domain)" -Type TXT
+            if ($DKIMDNSResolve.Strings) {
+                $DKIMDNSResults = $DKIMDNSResolve | Where-Object -Property "Type" -like "TXT"
+                $DKIMDNSLookup = [PSCustomObject]@{
+                    Name = $DKIMDNSResults.Name
+                    Strings = $DKIMDNSResults.Strings
+                }
+            }
+            $DKIMDetails | add-member -membertype NoteProperty "dkim_dnsresolve" -Value $DKIMDNSLookup
+
+            $Header_Details | add-member -membertype NoteProperty "dkim-signature" -Value $DKIMDetails
+        }
+        #>
         
         # If the record contains authentication-results
         if ($Headers.field -contains 'authentication-results') {
-            $Header_Details | add-member -membertype NoteProperty "authentication-results" -Value $($Headers | Where-Object -Property 'field' -like 'authentication-results' | Select-Object -ExpandProperty value)
+            $AuthResults = [PSCustomObject]@{
+                value = $($Headers | Where-Object -Property 'field' -like 'authentication-results' | Select-Object -ExpandProperty value)
+                spf = [PSCustomObject]@{
+                    status = $null
+                    summary = $null
+                }
+                dkim = [PSCustomObject]@{
+                    status = $null
+                    summary = $null
+                }
+                dmarc = [PSCustomObject]@{
+                    status = $null
+                    action = $null
+                }
+            }
+            $AuthSummary = ([regex]::Matches($AuthResults.value, '^(spf=(?<spf_result>[^\s]+)\s\((?<spf_reason>.*?)\))?.*?;.*?\s(dkim=(?<dkim_result>[^\s]+)\s\((?<dkim_reason>.*?)\)).*?;.*?\s(dmarc=(?<dmarc_result>[^\s]+)\saction=(?<dmarc_action>\S+))?.*?$'))
+            
+            if ($AuthSummary[0].Groups['spf_result'].Value) {
+                $AuthResults.spf.status = $AuthSummary[0].Groups['spf_result'].Value
+                $AuthResults.spf.summary = $AuthSummary[0].Groups['spf_reason'].Value
+            }
+
+            if ($AuthSummary[0].Groups['dkim_result'].Value) {
+                $AuthResults.dkim.status = $AuthSummary[0].Groups['dkim_result'].Value
+                $AuthResults.dkim.summary = $AuthSummary[0].Groups['dkim_reason'].Value
+            }
+
+            if ($AuthSummary[0].Groups['dmarc_result'].Value) {
+                $AuthResults.dmarc.status = $AuthSummary[0].Groups['dmarc_result'].Value
+                $AuthResults.dmarc.action = $AuthSummary[0].Groups['dmarc_action'].Value
+            }
+
+            $Header_Details | add-member -membertype NoteProperty "authentication-results" -Value $AuthResults
         }
 
         # If the record contains X-Hashtags
