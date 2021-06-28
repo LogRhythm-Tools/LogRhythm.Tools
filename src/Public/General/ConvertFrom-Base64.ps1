f0e4e6526e72104e56740adc0267ec4ab20bf749
Jt3kt <github.jtekt@gmail.com>
2021-06-28 07:28:07 -0600

    Update ConvertFrom-Base64.ps1
    Add support for additional encoding sets for Converting Data from Base64.

--- src/Public/General/ConvertFrom-Base64.ps1
+++ src/Public/General/ConvertFrom-Base64.ps1
@@ -7,18 +7,56 @@ function ConvertFrom-Base64 {
         .INPUT
             System.String. You can pipe a string into ConvertFrom-Base64.
         .OUTPUT
-            System.String. ConvertFrom-Base64 returns a Unicode encoded string.
+            System.String. ConvertFrom-Base64 returns a encoded string.
         .EXAMPLE
             PS C:\> ConvertFrom-Base64 -String "TQBpAGMAaABhAGUAbAA="
             Michael
         .NOTES
             Author:
             Michael West
+
+            Updated By:
+            Eric Hart
     #>
     [OutputType('System.String')]
     param(
-        [string]$String
-    )
+        [string]$String,
 
-    [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($String))
+        [Parameter(ValueFromPipeline=$false)]
+        [ValidateSet(
+            'BigEndianUnicode',
+            'unicode',
+            'utf7',
+            'utf8', 
+            'utf32', 
+            ignorecase=$true
+        )]
+        [string]$Encoding
+    )
+    Begin {
+    }
+
+    Process {
+        switch ($Encoding) {
+            unicode {
+                [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($String))
+            }
+            utf7 {
+                [System.Text.Encoding]::UTF7.GetString([System.Convert]::FromBase64String($String))
+            }
+            utf8 {
+                [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($String))
+            }
+            utf32 {
+                [System.Text.Encoding]::UTF32.GetString([System.Convert]::FromBase64String($String))
+            }
+            bigendianunicode {
+                [System.Text.Encoding]::BigEndianUnicode.GetString([System.Convert]::FromBase64String($String))
+            }
+            Default {
+                [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($String))
+            }
+        }
+    }
+    
 }
