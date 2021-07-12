ba0901c74bb0310e8891f56c6d76c521143ee988
Jt3kt <github.jtekt@gmail.com>
2021-07-12 12:52:20 -0600

    Update Get-LrLists.ps1
    Correct pagination.  Add support for new Parameter, List Status.  Status defaults to only returning Active lists and supports Retired, All as alternative arguments.

--- src/Public/LogRhythm/Admin/Lists/Get-LrLists.ps1
+++ src/Public/LogRhythm/Admin/Lists/Get-LrLists.ps1
@@ -11,18 +11,22 @@ Function Get-LrLists {
 
         [NOTE]: Due to the way LogRhythm REST API is built, if the specified MaxItemsThreshold
         is less than the number of actual items in the list, this cmdlet will return an http 400 error.
-    .PARAMETER Credential
-        PSCredential containing an API Token in the Password field.
     .PARAMETER Name
         [System.String] (Name or Guid) or [System.Guid]
         Specifies a LogRhythm list object by providing one of the following property values:
           + List Name (as System.String), e.g. "LogRhythm: Suspicious Hosts"
           + List Guid (as System.String or System.Guid), e.g. D378A76F-1D83-4714-9A7C-FC04F9A2EB13
+    .PARAMETER Status
+        Specifies a LogRhythm list status.  Default behavior is to return only Active lists.
+
+        Valid entries: Active, Retired, All
     .PARAMETER MaxItemsThreshold
         The maximum number of list items to retrieve from LogRhythm.
         The default value for this parameter is set to 1001.
     .PARAMETER Exact
         Switch to force PARAMETER Name to be matched explicitly.
+    .PARAMETER Credential
+        PSCredential containing an API Token in the Password field.
     .INPUTS
         The Name parameter can be provided via the PowerShell pipeline.
     .OUTPUTS
@@ -62,8 +66,18 @@ Function Get-LrLists {
         [ValidateNotNull()]
         [string] $Name,
 
-        
+
         [Parameter(Mandatory = $false, Position = 1)]
+        [ValidateSet(
+            'Active',
+            'Retired', 
+            'All',
+            ignorecase=$true
+        )]
+        [string] $Status = 'Active',
+        
+
+        [Parameter(Mandatory = $false, Position = 2)]
         [ValidateSet(
             'application',
             'classification', 
@@ -87,19 +101,19 @@ Function Get-LrLists {
         [string] $ListType,
 
 
-        [Parameter(Mandatory = $false, Position = 2)]
+        [Parameter(Mandatory = $false, Position = 3)]
         [switch] $Exact,
 
         
-        [Parameter(Mandatory = $false, Position = 3)]
+        [Parameter(Mandatory = $false, Position = 4)]
         [ValidateRange(1,1000)]
         [int] $PageSize = 1000,
 
 
-        [Parameter(Mandatory = $false, Position = 4)]
+        [Parameter(Mandatory = $false, Position = 5)]
         [int] $PageNumber = 1,
 
-        [Parameter(Mandatory = $false, Position = 5)]
+        [Parameter(Mandatory = $false, Position = 6)]
         [ValidateNotNull()]
         [pscredential] $Credential = $LrtConfig.LogRhythm.ApiKey
     )
@@ -132,7 +146,7 @@ Function Get-LrLists {
 
         # Page requested for Results from API
         if ($PageNumber) {
-            $Headers.Add("PageNumber", $PageNumber)
+            $Headers.Add("pageNumber", $PageNumber)
         }
 
         if ($ListTypeValid) { 
@@ -176,15 +190,16 @@ Function Get-LrLists {
             $ErrorObject.Raw = $_
             return $ErrorObject
         }
+        
 
         # Pagination
         if ($Response.Count -eq $PageSize) {
+            Write-Verbose 'Begin Pagination'
             DO {
                 # Increment Page Count / Offset
                 $PageNumber = $PageNumber + 1
-                $Offset = ($PageNumber -1) * $PageSize
                 # Update Header Pagination Paramater
-                $Headers.PageNumber = $Offset
+                $Headers.PageNumber = $PageNumber
                 
                 # Retrieve Query Results
                 try {
@@ -202,23 +217,34 @@ Function Get-LrLists {
                 # Append results to Response
                 $Response = $Response + $PaginationResults
             } While ($($PaginationResults.Count) -eq $PageSize)
+            Write-Verbose 'End Pagination'
+        }
+
+        # Filter lists based on Status.  Default behavior is to return all active lists.
+        $Results = [List[Object]]::new()
+        ForEach ($ListResult in $Response) {
+            if ($Status -like 'active' -or $Status -like 'retired') {
+                if ($ListResult.status -like $Status) {
+                    $Results.add($ListResult) | Out-Null
+                }
+            } else {
+                $Results.add($ListResult) | Out-Null
+            }
         }
         
-
         # [Exact] Parameter
         # Search "Malware" normally returns both "Malware" and "Malware Options"
         # This would only return "Malware"
         if ($Exact) {
             $Pattern = "^$Name$"
-            $Response | ForEach-Object {
+            $Results | ForEach-Object {
                 if(($_.name -match $Pattern) -or ($_.name -eq $Name)) {
                     Write-Verbose "[$Me]: Exact list name match found."
-                    $List = $_
-                    return $List
+                    return $_
                 }
             }
         } else {
-            return $Response
+            return $Results
         }
     }
 
