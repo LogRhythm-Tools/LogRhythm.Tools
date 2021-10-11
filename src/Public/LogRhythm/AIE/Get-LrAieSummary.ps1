13dfcc5d795f6d645bb800233288989e033c2ef0
John Berkers <jberkers@ipsec.com.au>
2021-10-11 11:44:57 +1100

    Full sync with aiedrilldown

--- src/Public/LogRhythm/AIE/Get-LrAieSummary.ps1
+++ src/Public/LogRhythm/AIE/Get-LrAieSummary.ps1
@@ -158,13 +158,14 @@ Function Get-LrAieSummary {
         foreach ($ruleBlock in $_dd.RuleBlocks) {
             foreach ($field in $ruleBlock.DDSummaries) {
                 $fields = [PSCustomObject]@{
-                    FieldName = $($field.PIFType)
+                    FieldName = $($field.PIFType | Get-PIFTypeName)
                     FieldValue = ($field.DrillDownSummaryLogs | ConvertFrom-Json).field
                     FieldCount = ($field.DrillDownSummaryLogs | ConvertFrom-Json).value
                     RuleBlockId = ($ruleBlock.RuleBlockId)
                 }
+                $SummaryFields.Add($fields)
             }
-            $SummaryFields.Add($fields)
+
         }
 
         # Done!
