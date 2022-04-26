7ca05f4cff77d277da95801fedead97e76d4f462
Jt3kt <github.jtekt@gmail.com>
2022-04-26 03:54:45 -0600

    Update Enable-LrIdentityIdentifier.ps1
    Apply use of Invoke-RestAPIMethod for Lr Cmdlets

--- src/Public/LogRhythm/Admin/Identities/Enable-LrIdentityIdentifier.ps1
+++ src/Public/LogRhythm/Admin/Identities/Enable-LrIdentityIdentifier.ps1
@@ -99,6 +99,8 @@ Function Enable-LrIdentityIdentifier {
     )
 
     Begin {
+        $Me = $MyInvocation.MyCommand.Name
+
         # Request Setup
         $BaseUrl = $LrtConfig.LogRhythm.BaseUrl
         $Token = $Credential.GetNetworkCredential().Password
@@ -128,7 +130,7 @@ Function Enable-LrIdentityIdentifier {
         }
 
         # Establish Body Contents
-        $BodyContents = [PSCustomObject]@{
+        $Body = [PSCustomObject]@{
             recordStatus = "Active"
         } | ConvertTo-Json
         
@@ -141,17 +143,9 @@ Function Enable-LrIdentityIdentifier {
         # Send Request and proceed if Identifier is Present
         if ($IdentifierStatus.IsPresent -eq $True -and $IdentifierStatus.RecordStatus -eq "Retired") {
             # Send Request
-            try {
-                $Response = Invoke-RestMethod $RequestUrl -Headers $Headers -Method $Method -Body $BodyContents
-            }
-            catch [System.Net.WebException] {
-                $Err = Get-RestErrorMessage $_
-                $ErrorObject.Error = $true
-                $ErrorObject.Type = "System.Net.WebException"
-                $ErrorObject.Code = $($Err.statusCode)
-                $ErrorObject.Note = $($Err.message)
-                $ErrorObject.Raw = $_
-                return $ErrorObject
+            $Response = Invoke-RestAPIMethod -Uri $RequestUrl -Headers $Headers -Method $Method -Body $Body -Origin $Me
+            if ($Response.Error) {
+                return $Response
             }
         } else {
             return $IdentifierStatus
