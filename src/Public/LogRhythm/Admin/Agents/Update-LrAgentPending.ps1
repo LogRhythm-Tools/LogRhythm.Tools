204c7f5ed5708d2c121a2f5f168b73d63f5c3751
Eric H <github.jtekt@gmail.com>
2022-07-29 07:22:12 -0400

    Update Update-LrAgentPending.ps1
    Provides the ability to associate or reject a pending Sysmon agent.

--- src/Public/LogRhythm/Admin/Agents/Update-LrAgentPending.ps1
+++ src/Public/LogRhythm/Admin/Agents/Update-LrAgentPending.ps1
@@ -52,22 +52,134 @@ Function Update-LrAgentPending {
 
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
-        [string] $Guid,
-
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, Position = 0)]
+        [string] $Entity,
 
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 1)]
         [ValidateSet('accept','associate', 'reject', ignorecase=$true)]
         [string] $AcceptanceStatus,
 
+        [Parameter(Mandatory = $false, Position = 4)]
+        [int] $AssociateAgentId,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, Position = 1)]
+        [ValidateSet('SystemMonitor','SystemMonitorBasic', 'SystemMonitorCollector', ignorecase=$true)]
+        [string] $LicenseType,
+
         [Parameter(Mandatory = $false, Position = 2)]
-        [int] $AgentId,
-        
+        [bool] $OverrideEntityForNewHosts = $false,
+
         [Parameter(Mandatory = $false, Position = 3)]
+        [int] $ConfigPolicy,
+
+        [Parameter(Mandatory = $false, Position = 4)]
+        [string] $Guid,
+
+        [Parameter(Mandatory = $false, Position = 5)]
+        [int] $MediatorId,
+
+        [Parameter(Mandatory = $false, Position = 6)]
+        [int] $Priority,
+
+        [Parameter(Mandatory = $false, Position = 7)]
+        [int] $ClientAddress = 0,
+
+        [Parameter(Mandatory = $false, Position = 8)]
+        [int] $ClientPort = 0,
+
+        [Parameter(Mandatory = $false, Position = 9)]
+        [int] $MediatorSSLPort = 443,
+
+        [Parameter(Mandatory = $false, Position = 10)]
+        [string] $MediatorIPv4,
+
+        [Parameter(Mandatory = $false, Position = 11)]
+        [string] $MediatorIPv6,
+
+        [Parameter(Mandatory = $false, Position = 12)]
+        [string] $MediatorFQDN,
+
+        [Parameter(Mandatory = $false, Position = 13)]
+        [int] $FimPolicies,
+        
+        [Parameter(Mandatory = $false, Position = 14)]
+        [int] $RealTimeRecordBufferLimit = 10485760,
+
+        [Parameter(Mandatory = $false, Position = 15)]
+        [int] $RealtimeIDMPollingInterval = 5,
+
+        [Parameter(Mandatory = $false, Position = 19)]
+        [bool] $RTFIMExcludeNestedDirectoryEvents = $false,
+
+        [Parameter(Mandatory = $false, Position = 16)]
+        [bool] $FIMEnabled = $false,
+
+        [Parameter(Mandatory = $false, Position = 17)]
+        [bool] $DataDefender = $false,
+
+        [Parameter(Mandatory = $false, Position = 18)]
+        [bool] $RTFIM = $false,
+
+        [Parameter(Mandatory = $false, Position = 19)]
+        [bool] $RTFIMAnomalyDetection = $false,
+        
+
+        [Parameter(Mandatory = $false, Position = 20)]
+        [bool] $RIMEnabled = $false,
+
+        [Parameter(Mandatory = $false, Position = 21)]
+        [int] $RIMPolicies,
+
+        [Parameter(Mandatory = $false, Position = 22)]
+        [int] $DLDPolicy,
+
+        [Parameter(Mandatory = $false, Position = 23)]
+        [bool] $PMIncludeUAMData = $false,
+
+        [Parameter(Mandatory = $false, Position = 24)]
+        [bool] $PM = $false,
+
+        [Parameter(Mandatory = $false, Position = 25)]
+        [int] $PMInterval = 5,
+
+        [Parameter(Mandatory = $false, Position = 26)]
+        [bool] $NCM = $false,
+
+        [Parameter(Mandatory = $false, Position = 27)]
+        [bool] $NCMIncludeUAM = $false,
+
+        [Parameter(Mandatory = $false, Position = 28)]
+        [bool] $NCMLogListeners = $false,
+
+        [Parameter(Mandatory = $false, Position = 29)]
+        [bool] $NCMLogInbound = $false,
+
+        [Parameter(Mandatory = $false, Position = 30)]
+        [bool] $NCMLogOutbound = $false,
+
+        [Parameter(Mandatory = $false, Position = 31)]
+        [int] $NCMInterval = 5,
+
+        [Parameter(Mandatory = $false, Position = 32)]
+        [bool] $UAMLogonActivity = $false,
+
+        [Parameter(Mandatory = $false, Position = 33)]
+        [bool] $UAMProcessActivity = $false,
+
+        [Parameter(Mandatory = $false, Position = 34)]
+        [bool] $UAMSessionActivity = $false,
+
+        [Parameter(Mandatory = $false, Position = 35)]
+        [int] $UAMInterval = 5,
+
+        [Parameter(Mandatory = $false, Position = 36)]
+        [int] $UAMHistory = 1,
+
+        [Parameter(Mandatory = $false, Position = 37)]
         [switch] $PassThru,
 
 
-        [Parameter(Mandatory = $false, Position = 4)]
+        [Parameter(Mandatory = $false, Position = 38)]
         [ValidateNotNull()]
         [pscredential] $Credential = $LrtConfig.LogRhythm.ApiKey
     )
@@ -88,6 +200,9 @@ Function Update-LrAgentPending {
 
         # Check preference requirements for self-signed certificates and set enforcement for Tls1.2 
         Enable-TrustAllCertsPolicy
+
+        # Integer Reference
+        [int32] $_int = 1
     }
 
     Process {
@@ -111,7 +226,29 @@ Function Update-LrAgentPending {
             return $ErrorObject
         }
 
+        # Lookup Entity
+        if ($Entity) {
+            if ([int]::TryParse($Entity, [ref]$_int)) {
+                Write-Verbose "[$Me]: Entity parses as integer."
+                $_entity = Get-LrEntityDetails -Id $Entity
+            } else {
+                Write-Verbose "[$Me]: Id does not parse as integer.  Performing string lookup."
+                $EntityLookup = Get-LrEntities -Name $Entity -Exact
+                if ($EntityLookup.Error -eq $true) {
+                    $ErrorObject.Error = $EntityLookup.Error
+                    $ErrorObject.Type = $EntityLookup.Type
+                    $ErrorObject.Code = $EntityLookup.Code
+                    $ErrorObject.Note = $EntityLookup.Note
+                    $ErrorObject.Value = $Entity
+                    return $ErrorObject
+                } else {
+                    $_entity = $EntityLookup
+                }
+            }
+        }
+
         # Check if Guid value is a valid Guid
+        
         if (Test-Guid $Guid) {
             Write-Verbose "[$Me]: Guid parses as valid guid."
         } else {
@@ -128,9 +265,11 @@ Function Update-LrAgentPending {
                     return $ErrorObject
                 } else {
                     $Guid = $AgentLookup.guid
+                    $SystemMonitorId = $AgentLookup.id
                 }
             } else {
                 $Guid = $AgentLookup.guid
+                $SystemMonitorId = $AgentLookup.id
             }
         }
 
@@ -139,21 +278,104 @@ Function Update-LrAgentPending {
             # Define Query URL
             $RequestUrl = $BaseUrl + "/lr-admin-api/agents-request/" + $Guid + "/reject"
         } elseif ($AcceptanceStatus -like 'associate') {
-            if (!$AgentId) {
-                $ErrorObject.Error = $true
-                $ErrorObject.Code = 404
-                $ErrorObject.Raw = $AgentId
-                $ErrorObject.Type = "Input.Validation.Lookup"
-                $ErrorObject.Note = "Guid String [$AgentId] not found in Accepted Agents."
-                return $ErrorObject
+            if ([int]::TryParse($AssociateAgentId, [ref]$_int)) {
+                Write-Verbose "[$Me]: AssociateAgentId parses as int."
+                $AssociateAgentLookup = Get-LrAgentDetails -Id $AssociateAgentId
+                if (($null -ne $AssociateAgentLookup.Error) -and ($AssociateAgentLookup.Error -eq $true)) {
+                    return $AssociateAgentLookup
+                }
+                $_associateAgentId = $AssociateAgentId
+            } else {
+                Write-Verbose "[$Me]: AssociateAgentId does not parse as Int.  Performing string lookup."
+                $AssociateAgentLookup = Get-LrAgentsAccepted -Name $AssociateAgentId -Exact
+                if (!$AssociateAgentLookup) {
+                        $ErrorObject.Error = $true
+                        $ErrorObject.Code = 404
+                        $ErrorObject.Raw = $AssociateAgentId
+                        $ErrorObject.Type = "Input.Validation.Lookup"
+                        $ErrorObject.Note = "Agent name [$AssociateAgentId] not found in Accepted Agents."
+                        return $ErrorObject
+                } elseif (($null -ne $AssociateAgentLookup.Error) -and ($AssociateAgentLookup.Error -eq $true)) {
+                    return $AssociateAgentLookup
+                } else {
+                    $_associateAgentId = $AssociateAgentLookup.id
+                }
             }
+
             $Body  = [PSCustomObject]@{
-                agentId = $AgentId
+                agentId = $_associateAgentId
             } | ConvertTo-Json -Compress
     
             # Request URL
             $RequestUrl = $BaseUrl + "/lr-admin-api/agents-request/$Guid/associate"
         } else {
+            $ErrorObject.Error = $true
+            $ErrorObject.Code = 500
+            $ErrorObject.Raw = $SystemMonitorId
+            $ErrorObject.Type = "Accept.Reject"
+            $ErrorObject.Note = "Accepting an agent is currently not available in this version of LogRhythm.Tools."
+            return $ErrorObject
+
+            $Body = [PSCustomObject]@{
+                AgentType = 'Windows'
+                overrideEntityForNewHosts = $OverrideEntityForNewHosts
+                entity = [PSCustomObject]@{
+                    name = $_entity.name
+                    id = $_entity.id
+                }
+                licenseType = $LicenseType
+                searchScope = 'ParentEntitySearch'
+                syslogEnabled = $true
+                #smConfigPolicy = $_smPolicyId
+                agentToMediators = @( [PSCustomObject]@{
+                    systemMonitorID = $SystemMonitorId
+                    mediator = [PSCustomObject]@{
+                        id = 1
+                        name = 'g7-test'
+                    }
+                    priority = 1
+                    clientAddress = "192.168.5.4"
+                    clientPort = 33333
+                    serverSSLPort = 443
+                    serverIP = "10.1.0.80"
+                    serverIPv6 = "2001:0db8:5b96:0000:0000:426f:8e17:642a"
+                    serverDNS = "test.logrhythm.cloud"
+                })
+                #fimPolicies = $_fimPolicyId
+                fileIntegrityMonitor = [PSCustomObject]@{
+                    fileMonitorEnabled = $FIMEnabled # bool
+                    dataDefender = $DataDefender # bool
+                    realtimeFileMonitor = $RTFIM # bool
+                    realtimeAnomalyDetection = $RTFIMAnomalyDetection # bool
+                    realtimeRecordBufferLimit = $_Int_realtimeRecordBufferLimit # 0 to 2147483647, def 10485760
+                    realtimeIDMPollingInterval = $RealtimeIDMPollingInterval # 5 to 86400, def 5
+                    RTFIMExcludeNestedDirectoryEvents = $RTFIMExcludeNestedDirectoryEvents # bool
+                }
+                rimEnabled = $RIMEnabled
+                #rimPolicies = @($_i_rimPolicies)
+                dldPolicy = -1
+                processMonitor = [PSCustomObject]@{
+                    pmincludeUAMData = $PMIncludeUAMData
+                    processMonitor = $PM
+                    processMonitorInterval = $PMInterval # 1 to 86400, def 5
+                }
+                networkConnectionMonitor = [PSCustomObject]@{
+                    networkConnectionMonitor = $NCM
+                    ncmIncludeUAMData = $NCMIncludeUAM
+                    logListeners = $NCMLogListeners
+                    logEstablishedInboundConnections = $NCMLogInbound
+                    logEstablishedOutboundConnections = $NCMLogOutbound
+                    networkConnectionMonitorInterval = $NCMInterval # 1 to 86400, def 5
+                }
+                userActivityMonitor = [PSCustomObject]@{
+                    logonActivity = $UAMLogonActivity
+                    processActivity = $UAMProcessActivity
+                    networkSessionActivity = $UAMSessionActivity
+                    interval = $UAMInterval # 3 to 86400
+                    history = $UAMHistory # 1 to 24
+                }
+            } | ConvertTo-Json -Depth 8 -Compress
+
             # Define Query URL
             $RequestUrl = $BaseUrl + "/lr-admin-api/agents-request/" + $Guid + "/accept"
         }
@@ -163,14 +385,14 @@ Function Update-LrAgentPending {
         if ($AcceptanceStatus -like "reject") {
             # Send Request
             $Response = Invoke-RestAPIMethod -Uri $RequestUrl -Headers $Headers -Method $Method -Origin $Me
-            if ($Response.Error) {
+            if (($null -ne $Response.Error) -and ($Response.Error -eq $true)) {
                 return $Response
             }
         } else {
             Write-Verbose "[$Me]: Request Body:`n$Body"
             # Send Request
             $Response = Invoke-RestAPIMethod -Uri $RequestUrl -Headers $Headers -Method $Method -Body $Body -Origin $Me
-            if ($Response.Error) {
+            if (($null -ne $Response.Error) -and ($Response.Error -eq $true)) {
                 return $Response
             }
         }
